fa193bd8e3e3c597c429359ad9f01ff5314fed46
ion <mdruh3@gmail.com>
2023-07-04 16:43:39 +0900

    OPERATOR

---
+++ 4.assignment.js
@@ -0,0 +1,18 @@
+// 할당 연산자(Assignment operators)
+let a = 1;
+a = a+2;
+console.log(a);
+
+a += 2; // a = a+2;
+console.log(a);
+
+a -=2;
+console.log(a);
+a *=2;
+console.log(a);
+a /=2;
+console.log(a);
+a %=2;
+console.log(a);
+a **=2;
+console.log(a);
